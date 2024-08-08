d4b15e963e43150f58007800d983d3c7da5ac877
Pablo Papes <pablopapes@gmail.com>
2024-08-08 15:33:48 -0300

    add telegram bot

--- main.go
+++ main.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/gocolly/colly"
+	"github.com/joho/godotenv"
 )
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 type Article struct {
 	Title string
@@ -17,6 +31,13 @@ func main() {
 	urlInfobae := args[1]
 	urlLaNacion := args[2]
 	collector := colly.NewCollector()
+
+	bot, err := tgbotapi.NewBotAPI(goDotEnvVariable("API_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = false
 
 	collector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
@@ -37,6 +58,12 @@ func main() {
 				article.Url = e.ChildAttr(".headline-link", "href")
 				fmt.Println(article.Title)
 				fmt.Println(urlInfobae + article.Url)
+
+				msg := tgbotapi.NewMessageToChannel("@news_argy", article.Title+"\n"+"👉 <a href='"+urlInfobae+article.Url+"'>Link a infobae</a>")
+				msg.ParseMode = "HTML"
+				msg.DisableWebPagePreview = true
+				bot.Send(msg)
+
 			}
 		})
 	})
@@ -49,9 +76,15 @@ func main() {
 			article.Url = e.ChildAttr("section", "href")
 			fmt.Println(article.Title)
 			fmt.Println(urlLaNacion + article.Url)
+
+			msg := tgbotapi.NewMessageToChannel("@news_argy", article.Title+"\n"+"👉 <a href='"+urlInfobae+article.Url+"'>Link a La Nación</a>")
+			msg.ParseMode = "HTML"
+			msg.DisableWebPagePreview = true
+			bot.Send(msg)
 		})
 	})
 
 	collector.Visit(urlInfobae)
 	collector.Visit(urlLaNacion)
+
 }
